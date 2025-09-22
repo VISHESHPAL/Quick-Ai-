4deb6bf18699e676e6e3cbcb9201ea7c7d656a1d
VISHESHPAL <156537104+VISHESHPAL@users.noreply.github.com>
2025-09-22 10:31:59 +0530

    the frontned is completed of the quick ai page lets go for the bakend

--- frontend/src/pages/GenerateImage.jsx
+++ frontend/src/pages/GenerateImage.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
+import { Image, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
 const GenerateImage = () => {
-  return (
-    <div>
-        <h1>GenerateImage</h1>
-    </div>
-  )
-}
+  const imageStyle = [
+    "Realistic",
+    "Ghibli style",
+    "Anime style",
+    "Cartoon style",
+    "Fantasy style",
+    "Realistic style",
+    "3D style",
+    "Portrait style",
+  ];
 
-export default GenerateImage
+  const [selectedStyle, setSelectedStyle] = useState("Realistic");
+  const [input, setInput] = useState("");
+  const [publish , setPublish] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="flex items-start flex-wrap h-full overflow-y-scroll p-6 gap-4 text-slate-700">
+      {/* Left  col  */}
+
+      <form
+        onSubmit={handleSubmit}
+        className=" w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#00ad25]" />
+          <h1 className="text-xl font-semibold">AI Image Generator </h1>
+        </div>
+        <p className="mt-6 text-sm font-medium ">Describe Your Image</p>
+        <textarea
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          rows={4}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-200"
+          placeholder="Describe what you want to see in the image ... "
+          required
+        />
+
+        <p className="mt-6 text-sm font-medium ">Style </p>
+
+        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
+          {imageStyle.map((item) => (
+            <span
+              onClick={() => setSelectedStyle(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedStyle === item
+                  ? "  bg-green-50 text-green-700"
+                  : "text-gray-500 border-gray-300"
+              }`}
+              key={item}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+
+
+        <div className="my-6 flex items-center gap-2 ">
+          <label className="relative cursor-pointer">
+             <input type="checkbox" onChange={(e) => setPublish(e.target.checked)} checked={publish}  className="sr-only peer"/>
+             <div className="w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition "></div>
+             <span className=" absolute h-3 w-3 bg-white rounded-full peer-checked:translate-x-4 transition  left-1 top-1 "> </span>
+          </label>
+
+          <p className="text-sm">  Make this image public </p>
+
+        </div>
+
+        <br />
+
+        <button
+          className=" w-full flex justify-center items-center gap-2
+              bg-gradient-to-r from-[#00ad25] to-[#04ff50] text-white px-4 py-2 mt-6
+              text-sm rounded-lg cursor-pointer"
+        >
+          <Image className="w-5 " />
+          Generate Image
+        </button>
+      </form>
+      {/* Right Col */}
+
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 ">
+        <div className="flex items-center gap-3">
+          <Image className="w-5 h-5 text-[#00ad25]" />
+          <h1 className="text-xl font-semibold">Generated Image</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Image className="w-9 h-9" />
+            <p>Enter a topic and click “Generate image ” to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GenerateImage;
--- frontend/src/pages/RemoveBackground.jsx
+++ frontend/src/pages/RemoveBackground.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import { Eraser, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
 const RemoveBackground = () => {
-  return (
-    <div>
-      <h1>RemoveBackground</h1>
-    </div>
-  )
-}
+  const [input, setInput] = useState("");
 
-export default RemoveBackground
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex items-start flex-wrap h-full overflow-y-scroll p-6 gap-4 text-slate-700">
+      {/* Left  col  */}
+
+      <form
+        onSubmit={handleSubmit}
+        className=" w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#ff4938]" />
+          <h1 className="text-xl font-semibold">Background Removal </h1>
+        </div>
+        <p className="mt-6 text-sm font-medium ">Upload image</p>
+        <input
+          onChange={(e) => setInput(e.target.files[0])}
+          type="file"
+          accept="image/*"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-200 text-gray-600"
+          placeholder="The future of artificial intelligence is ... "
+          required
+        />
+
+        <p className="text-sm text-gray-500 font-light mt-1">Supports JPG, PNG, and other image formats</p>
+
+        <button
+          className=" w-full flex justify-center items-center gap-2
+              bg-gradient-to-r from-[#f6ab41] to-[#ff4938] text-white px-4 py-2 mt-6
+              text-sm rounded-lg cursor-pointer"
+        >
+          <Eraser className="w-5 " />
+          Generate Title
+        </button>
+      </form>
+      {/* Right Col */}
+
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 ">
+        <div className="flex items-center gap-3">
+          <Eraser className="w-5 h-5 text-[#ff4938]" />
+          <h1 className="text-xl font-semibold">Processed Image</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Eraser className="w-9 h-9" />
+            <p>Upload an image and click "Remove Background" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RemoveBackground;
